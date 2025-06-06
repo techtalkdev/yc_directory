aed5ec9870d586b44b714e16b3926ff0ef185135
Mike <binarybardcode@gmail.com>
2025-06-06 15:28:53 +0200

    feat: add Sanity schema, queries, and typegen setup

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -5,9 +5,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { Author, Startup } from '@/sanity/types'
+
+export type StartupTypeCard = Omit<Startup, 'author'> & { author?: Author }
 
 const StartupCard = ({ post }:{ post: StartupTypeCard }) => {
-    const { _createdAt, views, author: { _id: authorId, name }, _id, description, image, category, title} = post;
+    const { 
+        _createdAt, 
+        views, 
+        author,
+        _id, 
+        description, 
+        image, 
+        category, 
+        title
+    } = post;
   return (
     <li className='startup-card group'>
         <div className='flex-between'>
@@ -20,9 +32,9 @@ const StartupCard = ({ post }:{ post: StartupTypeCard }) => {
 
         <div className='flex-between mt-5 gap-5'>
             <div className='flex-1'>
-                <Link href={`/user/${authorId}`}>
+                <Link href={`/user/${author?._id}`}>
                     <p className='text-16-medium line-clamp-1'>
-                        {name}
+                        {author?.name}
                     </p>
                 </Link>
                 <Link href={`/startup/${_id}`}>
@@ -31,7 +43,7 @@ const StartupCard = ({ post }:{ post: StartupTypeCard }) => {
                     </h3>
                 </Link>
             </div>
-            <Link href={`/user/${authorId}`}>
+            <Link href={`/user/${author?._id}`}>
                 <Image
                     src='https://placehold.co/48x48'
                     alt='placeholder'
@@ -48,7 +60,7 @@ const StartupCard = ({ post }:{ post: StartupTypeCard }) => {
         </Link>
 
         <div className='flex-between gap-3 mt-5'>
-            <Link href={`?query=${category.toLowerCase()}`}>
+            <Link href={`?query=${category?.toLowerCase()}`}>
                 <p className='text-16-medium'>{category}</p>
             </Link>
             <Button className='startup-card_btn' asChild>
